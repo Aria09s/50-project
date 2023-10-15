0ac7e24d4080002d5b23ccdf6d863dbd69c4d786
Aria <Arianaziri.79@gmail.com>
2023-10-15 17:17:39 +0300

    searchbar

---
+++ searchbar/js.js
@@ -0,0 +1,9 @@
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
+
+
+search = document.querySelector('.search')
+btn = document.querySelector('.btn')
+input = document.querySelector('.input')
