829a262b078078764016ba630e3eb54be134f492
Aria <Arianaziri.79@gmail.com>
2023-10-16 21:50:56 +0300

    Scroll Animation

---
+++ Scroll Animation/script.js
@@ -0,0 +1,29 @@
+// let box = document.querySelector ('.box');
+
+// box.addEventListener('scroll',show);
+
+// function show () { 
+// box.classList.add('show')
+// }
+
+
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
+
